ec4cf77bd564ff35e4018daf56cfa365801440a0
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-09-10 12:10:27 +0530

    add: orders, list dishes,add dishes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Sidebar from "./common/Sidebar";
+import Navbar from "./common/Navbar";
+import AddDishes from "./pages/AddDishes";
+import ListDishes from "./pages/ListDishes";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
-    <p>hello
-    </p>
-  )
+    <Router>
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <div className="w-[85%]">
+          <Navbar />
+          <div className="px-10 py-4">
+            <Routes>
+              <Route path="/add" element={<AddDishes />} />
+              <Route path="/list" element={<ListDishes />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/" element={<p>Welcome to the Admin Dashboard</p>} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
